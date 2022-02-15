c235349aa607bd9ca45a232c8aef3ff691c7b205
dmitriy melnik <dmelnik1981@mail.ru>
2022-02-15 13:18:34 +0500

    создал тело функции

--- search_bin.py
+++ search_bin.py
@@ -1,2 +1,21 @@
-def  search_bin():
-    
+def  search_bin(num, item):
+   low = 0
+   high = len(num)-1
+   while low <= high:
+         mid = (high-low)//2
+         if item == num[mid]:
+             return mid
+         if item < num[mid]:
+            high = mid-1
+         else:
+            low = mid+1
+   return None
+
+a = [1,3,5,7,9,11]
+x = 4
+print(search_bin(a,x))
+
+
+
+
+
